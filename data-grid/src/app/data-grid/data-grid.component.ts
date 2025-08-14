932ac47b0ff3bd621b3ab1c612955a926597a0a1
Sneha M Antonious <snea913@gmail.com>
2025-08-14 15:58:12 -0400

    Data Grid component creation

---
+++ data-grid/src/app/data-grid/data-grid.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-data-grid',
+  imports: [],
+  templateUrl: './data-grid.component.html',
+  styleUrl: './data-grid.component.css'
+})
+export class DataGridComponent {
+
+}
