d2ef3155f3d7d500613f09073628a1b216143c52
Sneha M Antonious <snea913@gmail.com>
2025-08-20 17:32:05 -0400

    Lazy Loading and Not Found page

--- eMiniStore/src/app/app.routes.ts
+++ eMiniStore/src/app/app.routes.ts
@@ -1,16 +1,51 @@
 import { Routes } from '@angular/router';
-import { ProductListComponent } from './products/product-list/product-list.component';
-import { ProductDetailsComponent } from './products/product-details/product-details.component';
-import { CartPageComponent } from './cart/cart-page.component';
+import { NotFoundComponent } from './shared/not-found.component';
+
 
 export const routes: Routes = [
-    // Home Page
-    { path: '', component: ProductListComponent },
 
-    //Product Details Page
-    { path: 'products/:id', component: ProductDetailsComponent },
+    //Lazy Loading Product page and its child routes
+    {
+        path: 'products',
+        loadComponent: () => import('./products/product-layout.component').then(m => m.ProductLayoutComponent),
+        children: [
+            {
+                path: '',
+                loadComponent: () => import('./products/product-list/product-list.component').then(m => m.ProductListComponent)
+            },
 
-    //Cart Page
-    { path: 'cart', component: CartPageComponent }
+            {
+                path: ':id',
+                loadComponent: () => import('./products/product-details/product-details.component').then(m => m.ProductDetailsComponent)
+            }
+        ]
+    },
+
+    //Lazy Loading Cart Page
+    {
+        path: 'cart',
+        loadComponent: () => import('./cart/cart-page.component').then(m => m.CartPageComponent),
+    },
+
+    //Default Route
+    {
+        path: '',
+        redirectTo: 'products',
+        pathMatch: 'full'
+    },
+
+    //Not Found Page - not lazy loaded because any invalid routes shoud be redirected to the products page- this logic is in NotFoundComponent
+    {
+        path:'**', component: NotFoundComponent
+    }
+
+
+    // { path: '', component: ProductListComponent },
+
+    // //Product Details Page
+    // { path: 'products/:id', component: ProductDetailsComponent },
+
+    // //Cart Page
+    // { path: 'cart', component: CartPageComponent }
 ];
 
---
+++ eMiniStore/src/app/products/product-layout.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+@Component({
+  selector: 'app-product-layout',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './product-layout.component.html',
+  styleUrls: ['./product-layout.component.css']
+})
+export class ProductLayoutComponent {
+
+}
